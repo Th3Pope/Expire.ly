fa9c19c63634cc8536c1f36d81f8cf92d571aafa
Nick Newhard <newhardn@cwu.edu>
2020-05-17 21:48:26 -0700

    This uses the newer version of expirely but also has the broken blue button - this is LIVE!

--- dbtest/assets/js/Create-Form.js
+++ dbtest/assets/js/Create-Form.js
@@ -20,24 +20,35 @@ function addValues() {
   var cell4 = row.insertCell(4);
   var cell5 = row.insertCell(5);
   cell5.align = "center";
+  var cell6 = row.insertCell(6); //this is row good idk y wont link
     
-  cell0.innerHTML = '<a class="btn btn-danger" onclick="deleteRow()"><em class="fa fa-trash" id="trash"></em></a>';
+  cell6.innerHTML = '<div align="center" style="font-size: 30px;"><i class="fas fa-circle" style="color: green;"></i></div>';
+    
+  cell0.innerHTML = '<a class="btn btn-danger" onclick="deleteRow(this)"><em class="fa fa-trash" id="trash"></em></a>';
     
   cell1.innerHTML = document.getElementById("foodName").value;
   cell2.innerHTML = document.getElementById("foodAmount").value;;
   cell3.innerHTML = document.getElementById("foodExpiration").value;
   cell4.innerHTML = document.getElementById("foodDescription").value;
-    
+
   var newImage = document.createElement("IMG");
   newImage.src = URL.createObjectURL(document.getElementById("foodImage").files[0]);
   newImage.height = "70";
   newImage.width = "70";
   cell5.appendChild(newImage);
-
+    
+  //cell6.innerHTML = document.getElementById("goodYeah").value;
+  
+ 
 }
-
-function deleteRow(input) {
-    document.getElementById("myTable").deleteRow(1);
+//this always deletes row 1 no matter what
+//function deleteRow(input) {
+   // var i = r.parentNode.parentNode.rowIndex;
+    //document.getElementById("myTable").deleteRow(input);// was 1 needs the input 
+//}
+function deleteRow(btn) {
+  var row = btn.parentNode.parentNode;
+  row.parentNode.removeChild(row);
 }
 
 function readURL(input) {
