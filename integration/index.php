26c553220f08cb019602a693b608b66e87887bc6
Nick Newhard <newhardn@cwu.edu>
2020-05-27 17:49:40 -0700

    new title

--- integration/index.php
+++ integration/index.php
@@ -17,7 +17,7 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-      <title>BlueButton</title>
+      <title>expire.ly</title>
       <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
       <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
       <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
