66772b3d850134fcbb23ba9e59dc1953a6d571ac
AustinHolloway <austinholloway007@gmail.com>
2020-06-01 11:02:53 -0700

    removed acouple lines we didnt need

--- integration/assets/js/Create-Form.js
+++ integration/assets/js/Create-Form.js
@@ -1,10 +1,4 @@
 
-      var today = new Date();
-      var dd = String(today.getDate()).padStart(2, '0');
-      var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-      var yyyy = today.getFullYear();
-      today = mm + '/' + dd + '/' + yyyy;
-
 function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
