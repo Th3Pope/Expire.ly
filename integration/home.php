d5ea285405058325ce72e2f65ec0f8e4bb9fce77
Th3Pope <olsonly@cwu.edu>
2020-06-05 20:30:51 -0700

    i think its summer time!

--- integration/home.php
+++ integration/home.php
@@ -18,7 +18,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Document</title>
+    <title>Expire.ly</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cookie">
     <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
@@ -36,6 +36,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     <link rel="stylesheet" href="assets/css/Pretty-Header.css">
     <link rel="stylesheet" href="assets/css/Profile-Edit-Form-1.css">
     <link rel="stylesheet" href="assets/css/Profile-Edit-Form.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap" rel="stylesheet"> 
 </head>
 
 <body><nav class="navbar navbar-light navbar-expand-md custom-header">
@@ -62,64 +65,49 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
             </ul>
     </div>
     </div>
-</nav><script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+</nav>
+<html>
 
 
-<!-- <div class="top-bar">
-    <div class="logo">Expire.ly</div>
-    <div class="menu">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Calendar</a></li>
-      </ul>
-    </div>
-  </div> -->
-  <div class="banner">
-    <div class="banner-text">
-      <div class="banner-text-items">
-        <h2>We Are Expire.ly</h2>
-          <p>Your Pantry Solution</p>
-      </div>
-    </div>
-  </div>
-  <div class="main-content">
-    <div class="main1">
-      <h2>Here's a quick breakdown of your pantry</h2>
-        <!--canvas id="DoughnutChart" height="400" width="400">
- <script src="JavaScript/myChart.js"></script>
-            
-        </canvas-->
-       <canvas id="myChart1" width="300" height="300" style="display: block; width: 300px; height: 300px;"></canvas> 
-        
-        
-    </div>
-    <div class="main1">
-      <h2>Here is your to do list</h2>
-      <p>This is a list.
-        This is a list.</p>
-    </div>
-    <div class="main1">
-      <h2>What we are about</h2>
-      <p>Here at Expire.ly we are in the business of making sure you pantry is in order. Whether it be a stockpile being collected for a rainy day or you wanna make sure that steak in the freezer doesn't go to waste, we got you covered!</p>
-    </div>
-  </div>
+<body id="page-top">
+    <header class="masthead text-center text-white d-flex" 
+    style="background-image:url('assets/img/veggies.jpg'); background-size: cover; height: 700px;">
+        <div class="container my-auto">
+            <div class="row">
+                <div class="col-lg-10 mx-auto">
+                    <h1 style="padding-top: 100px; font-family: 'Rock Salt', cursive; font-size: 75px; padding-bottom: 50px;" ><strong>Welcome To Expire.ly</strong></h1>
+                    <hr>
+                </div>
+            </div>
+            <div class="col-lg-8 mx-auto">
+                <p style=" font-family: 'Roboto', sans-serif; font-size: 20px;" class="text-faded mb-5">We are dedicated to helping you make sure your kitchen is fully stocked, 
+                    and that nothing goes to waste. Have a look around you will be glad you did!
+                </p></div>
+        </div>
+    </header>
+    <section id="contact"style="background-color: #f9f3db;">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 mx-auto text-center">
+                    <h2 class="section-heading"style="padding-top: 20px; font-family: 'Permanent Marker', cursive;">Let's Get In Touch!</h2>
+                    <hr class="my-4">
+                    <p class="mb-5"style=" font-family: 'Roboto', sans-serif; font-size: 20px;">If you have any questions or concerns, don't hesitate. Give us a call or send us 
+                        an email and we will get back to you as soon as possible!</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-4 ml-auto text-center">
+                    <i class="fa fa-phone fa-3x mb-3 sr-contact" data-aos="zoom-in" data-aos-duration="300" data-aos-once="true"></i>
+                    <p style=" font-family: 'Roboto', sans-serif; font-size: 16px;">605-475-6968</p>
+                </div>
+                <div class="col-lg-4 mr-auto text-center"><i class="fa fa-envelope-o fa-3x mb-3 sr-contact" data-aos="zoom-in" data-aos-duration="300" data-aos-delay="300" data-aos-once="true"></i>
+                    <p><a href="mailto:your-email@your-domain.com" style=" font-family: 'Roboto', sans-serif; font-size: 16px;">CustomerCare@Expire.ly</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
 
-  <div class="contact">
-    <div class="contact">
-      <div class="contact-items">
-        <h2>Contact Us</h2>
-          <p>We wanna hear from you!</p>
-        <input type="text" name="" placeholder="Your Name...">
-        <input type="text" name="" placeholder="Your Email...">
-        <textarea rows="20" cols="20" placeholder="Your Message Here..."></textarea>
-        <a href="#">Submit</a>
-      </div>
-    </div>
-  </div>
-<!--script src="JavaScript/myChart.js"></script-->
-    <script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.min.js"></script>
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
     <script src="assets/js/Create-Form.js"></script>
     <script src="assets/js/myChart.js"></script>
